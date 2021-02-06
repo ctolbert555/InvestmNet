48829b1c5e02fce5953eab68a43c73e3c89df071
clussier <59848742+clussier@users.noreply.github.com>
2021-02-06 11:36:17 -0600

    created stack

--- Investment Strategy.py
+++ Investment Strategy.py
@@ -2,6 +2,7 @@ from TDSCoinbaseData import TDSCoinbaseData
 from TDSTickGenerator import TDSTickGenerator
 from TDSTransactionTracker import TDSTransactionTracker
 import logging
+import torch
 import numpy as np
 logging.getLogger().setLevel(level=logging.ERROR)
 
@@ -9,12 +10,15 @@ cb = TDSCoinbaseData()
 starts = ['20201001', '20200701', '20190601']
 ends = ['20201231', '20200930', '20190831']
 
+#Global variables
 index = 0
 start_date = starts[index]
 end_date = ends[index]
 products = ['BTC-USD', 'ETH-BTC', 'LTC-BTC', 'BTC-EUR']
 isHolding = True
 takers_fee = 0.0018
+# Initialize stack
+currency_stack = torch.zeros((4, 60, 5))
 
 print("Data grabbed")
 # Instantiate a tick generator
@@ -24,45 +28,10 @@ df_usd = cb.get_market_data(products[0], start_date, end_date, interval=60, over
 df_eth = cb.get_market_data(products[1], start_date, end_date, interval=60, overwrite=False)
 df_ltc = cb.get_market_data(products[2], start_date, end_date, interval=60, overwrite=False)
 df_eur = cb.get_market_data(products[3], start_date, end_date, interval=60, overwrite=False)
-
 print("Generator and Tracker created")
 
-#TEST findHigh
-down_slope = [188, 187, 186, 185, 184, 183]
-up_slope = [188, 189, 190, 191, 194, 50000]
-peak = [188, 189, 190, 191, 194, 195, 194, 193, 192]
-troph = [188, 187, 186, 185, 184, 183, 188, 189, 190, 191, 194, 195]
-# Helper function to find the highest ticker in a sequence
-def findHigh(tickers):
-    #TODO reconfigure for correct data storage type
-    highest_ticker = tickers[0]
-    for ticker in tickers:
-        if ticker > highest_ticker:
-            highest_ticker = ticker
-    return highest_ticker
-
-print("TEST findHigh: ")
-print("Down Slope: ", findHigh(down_slope))
-print("Up Slope: ", findHigh(up_slope))
-print("Peak: ", findHigh(peak))
-print("Trough: ", findHigh(troph))
-
-# Helper funciton to find the lowest ticker in a sequence
-def findLow(tickers):
-    #TODO reconfigure for correct data storage type
-    lowest_ticker = tickers[0]
-    for ticker in tickers:
-        if ticker < lowest_ticker:
-            lowest_ticker = ticker
-    return lowest_ticker
-
-print("TEST findLow: ")
-print("Down Slope: ", findLow(down_slope))
-print("Up Slope: ", findLow(up_slope))
-print("Peak: ", findLow(peak))
-print("Troph: ", findLow(troph))
-
-
+#------
+#------
 # Helper funciton to check if a trade should be profitable
 def isProfitable(isHolding, current_price, last_price):
     if isHolding:
@@ -74,39 +43,40 @@ def isProfitable(isHolding, current_price, last_price):
         if last_price >= (current_price * (1 - takers_fee)):
             return False
     return True
-
-
-# Function to check if we are currently holding bitcoin
+#------
+#------
+# Function to check if we are currently holding BTC
 def checkIfHolding():
     if trans_tracker.get_holdings()["BTC"] == 0.0:
         return False
     return True
 
-
+# Function to check if we are currently holding USD
 def checkIfUSD():
     if ("USD" not in trans_tracker.get_holdings()) or trans_tracker.get_holdings()["USD"] == 0.0:
         return False
     return True
 
+# Function to check if we are currently holding EUR
 def checkIfEUR():
     if ("EUR" not in trans_tracker.get_holdings()) or trans_tracker.get_holdings()["EUR"] == 0.0:
         return False
     return True
 
+# Function to check if we are currently holding ETH
 def checkIfETH():
     if ("ETH" not in trans_tracker.get_holdings()) or trans_tracker.get_holdings()["ETH"] == 0.0:
         return False
     return True
 
+# Function to check if we are currently holding LTC
 def checkIfLTC():
     if ("LTC" not in trans_tracker.get_holdings()) or trans_tracker.get_holdings()["LTC"] == 0.0:
         return False
     return True
-
-
+#------
+#------
 # Function that checks predictions to see if this ticker is a buy/sell
-
-
 def makeTrade(current_tick, tickers, last_price):
     usd_val = 1/tick.p.btc_usd.close
     eth_val = tick.p.eth_btc.close
@@ -170,34 +140,45 @@ def makeTrade(current_tick, tickers, last_price):
                 trans_tracker.make_trade(current_tick, 'BTC-EUR', 'buy', trans_tracker.get_holdings()["EUR"])
             else:
                 trans_tracker.make_trade(current_tick, 'BTC-EUR', 'buy', tick.p.btc_eur.volume * 0.499999 / eur_val)
-
-
+#------
+#------
 def model_predictions(frame, step=60):
+    # Volatility threshold
     threshold = 12
     # print(np.average(df_usd['volume'].values[frame:frame+step]))
-    if np.average(df_usd['volume'].values[frame:frame+step]) > 3 * threshold:
+    #TODO convert dataframe to tensor code
+    if np.average(df_usd['volume'].values[frame - step :frame]) > 3 * threshold:
         step = 15
-    elif np.average(df_usd['volume'].values[frame:frame+step]) > 2 * threshold:
+    elif np.average(df_usd['volume'].values[frame - step:frame]) > 2 * threshold:
         step = 20
-    elif np.average(df_usd['volume'].values[frame:frame+step]) > threshold:
+    elif np.average(df_usd['volume'].values[frame - step:frame]) > threshold:
         step = 30
 
     return np.stack((1/df_usd['close'].values[frame:frame+step],
                     df_eth['close'].values[frame:frame+step],
                     df_ltc['close'].values[frame:frame+step],
                     1/df_eur['close'].values[frame:frame+step]), 0)
+#------
+
 
 
 tick = tick_gen.get_tick()
 last_price = tick.p.btc_usd.close
-
+# Which frame we are currently on
 frame = 0
+# How many minutes we should wait before tradig (usually because we need enough data)
+wait = 60
 while tick is not None:
     tick = tick_gen.get_tick()
     frame += 1
-    if tick is not None:
-        #TODO get predicted tickers from NeuralNetwork
-        makeTrade(tick, model_predictions(frame), last_price)
+    #TODO Save tickers into tensor code so we can perform volatility calculations
+    if frame > wait:
+        if tick is not None:
+            #TODO get predicted tickers from NeuralNetwork
+            makeTrade(tick, model_predictions(frame), last_price)
+
+
+# Display findings
 trans_tracker.plot_btc_holdings()
 print(trans_tracker.get_holdings())
 print(trans_tracker.get_sharpe_ratio())
