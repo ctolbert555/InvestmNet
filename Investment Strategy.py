a59176d79602943eaa968bb9d2793a5d9d0a0df5
Corey Tolbert <ctolbert555@gmail.com>
2021-02-06 04:10:30 -0600

    Abandoned training of NN, focused on strategy

--- Investment Strategy.py
+++ Investment Strategy.py
@@ -2,24 +2,24 @@ from TDSCoinbaseData import TDSCoinbaseData
 from TDSTickGenerator import TDSTickGenerator
 from TDSTransactionTracker import TDSTransactionTracker
 import logging
-import numpy
+import numpy as np
 logging.getLogger().setLevel(level=logging.ERROR)
 
 cb = TDSCoinbaseData()
 
-start_date = '20200101'
+start_date = '20201001'
 end_date = '20201231'
-products = ['BTC-USD', 'ETH-USD', 'LTC-USD', 'ETH-BTC', 'LTC-BTC']
+products = ['BTC-USD', 'ETH-BTC', 'LTC-BTC']
 isHolding = True
 takers_fee = 0.0018
 
-for product in products:
-    ## Only use overwrite=True when backfilling data. Otherwise, either specify overwrite=False or omit the arg entirely
-    df = cb.get_market_data(product, start_date, end_date, interval=60, overwrite=False)
 print("Data grabbed")
 # Instantiate a tick generator
 tick_gen = TDSTickGenerator(cb, products, start_date, end_date, interval=60)
-trans_tracker = TDSTransactionTracker(start_date, end_date, holdings={'BTC' : 1.0})
+trans_tracker = TDSTransactionTracker(start_date, end_date, holdings={'BTC': 1.0})
+df_usd = cb.get_market_data(products[0], start_date, end_date, interval=60, overwrite=False)
+df_eth = cb.get_market_data(products[1], start_date, end_date, interval=60, overwrite=False)
+df_ltc = cb.get_market_data(products[2], start_date, end_date, interval=60, overwrite=False)
 
 print("Generator and Tracker created")
 
@@ -41,7 +41,7 @@ print("TEST findHigh: ")
 print("Down Slope: ", findHigh(down_slope))
 print("Up Slope: ", findHigh(up_slope))
 print("Peak: ", findHigh(peak))
-print("Troph: ", findHigh(troph))
+print("Trough: ", findHigh(troph))
 
 # Helper funciton to find the lowest ticker in a sequence
 def findLow(tickers):
@@ -58,18 +58,19 @@ print("Up Slope: ", findLow(up_slope))
 print("Peak: ", findLow(peak))
 print("Troph: ", findLow(troph))
 
-# # Helper funciton to check if a trade should be profitable
-# def isProfitable(isHolding, current_tick, last_price):
-#     if not isHolding:
-#         # When selling, and the last price is not greater than curent price plus the fee, return false
-#         if not last_price > (current_tick + takers_fee):
-#             return False
-#     else:
-#         # When buying, and the last price is not greater than curent price plus the fee, return false
-#         #TODO Fix this
-#         if not (last_price < (current_tick + takers_fee)):
-#             return False
-#     return True
+
+# Helper funciton to check if a trade should be profitable
+def isProfitable(isHolding, current_price, last_price):
+    if isHolding:
+        # When selling, and the last price is not greater than curent price plus the fee, return false
+        if last_price <= (current_price * (1 + takers_fee)):
+            return False
+    else:
+        # When buying, and the last price is not greater than curent price plus the fee, return false
+        if last_price >= (current_price * (1 - takers_fee)):
+            return False
+    return True
+
 
 # Function to check if we are currently holding bitcoin
 def checkIfHolding():
@@ -77,33 +78,93 @@ def checkIfHolding():
         return False
     return True
 
+
+def checkIfUSD():
+    if ("USD" not in trans_tracker.get_holdings()) or trans_tracker.get_holdings()["USD"] == 0.0:
+        return False
+    return True
+
+def checkIfETH():
+    if ("ETH" not in trans_tracker.get_holdings()) or trans_tracker.get_holdings()["ETH"] == 0.0:
+        return False
+    return True
+
+def checkIfLTC():
+    if ("LTC" not in trans_tracker.get_holdings()) or trans_tracker.get_holdings()["LTC"] == 0.0:
+        return False
+    return True
+
+
 # Function that checks predictions to see if this ticker is a buy/sell
 
-def makeTrade(isHold, current_tick, tickers):
-    ticker_price = tick.p.btc_usd.close
-    #TODO if looking to buy, True, otherwise false
-    if not checkIfHolding():
-        # Scenerio where current_tick is a trough
-        #TODO correct for numpy array
-        if(findLow(tickers) >= ticker_price):
-            trans_tracker.make_trade(current_tick, 'BTC-USD', 'buy', -1)
-    else:
-        # Scenerio where current_tick is peak
-        if findHigh(tickers) <= ticker_price:
-            trans_tracker.make_trade(current_tick, 'BTC-USD', 'sell', -1)
-       # Scenerio where in future there will be a better buy
 
+def makeTrade(current_tick, tickers, last_price):
+    usd_val = 1/tick.p.btc_usd.close
+    eth_val = tick.p.eth_btc.close
+    ltc_val = tick.p.ltc_btc.close
+    #TODO if looking to buy, True, otherwise false
+    if checkIfHolding():
+        # Scenario where current_tick is a trough
+        #TODO correct for numpy array
+        if tickers[0].min() >= usd_val:
+            # BUY USD
+            if tick.p.btc_usd.volume * 0.5 > trans_tracker.get_holdings()["BTC"]:
+                trans_tracker.make_trade(current_tick, 'BTC-USD', 'sell', -1)
+            else:
+                trans_tracker.make_trade(current_tick, 'BTC-USD', 'sell', tick.p.btc_usd.volume * 0.4999999)
+        if tickers[1].min() >= eth_val:
+            # BUY ETH
+            if tick.p.eth_btc.volume * 0.5 > trans_tracker.get_holdings()["BTC"] / eth_val:
+                trans_tracker.make_trade(current_tick, 'ETH-BTC', 'buy', -1)
+            else:
+                trans_tracker.make_trade(current_tick, 'ETH-BTC', 'buy', (tick.p.eth_btc.volume * 0.499999) * eth_val)
+        if tickers[2].min() >= ltc_val:
+            # BUY LTC
+            if tick.p.ltc_btc.volume * 0.5 > trans_tracker.get_holdings()["BTC"] / ltc_val:
+                trans_tracker.make_trade(current_tick, 'LTC-BTC', 'buy', -1)
+            else:
+                trans_tracker.make_trade(current_tick, 'LTC-BTC', 'buy', tick.p.ltc_btc.volume * 0.499999 * ltc_val)
+    if checkIfUSD():
+        # Scenario where current_tick is peak
+        if tickers[0].max() <= usd_val:
+            if tick.p.btc_usd.volume * 0.5 > trans_tracker.get_holdings()["USD"] * usd_val:
+                trans_tracker.make_trade(current_tick, 'BTC-USD', 'buy', -1)
+            else:
+                trans_tracker.make_trade(current_tick, 'BTC-USD', 'buy', tick.p.btc_usd.volume * 0.499999 / usd_val)
+    if checkIfETH():
+        # Scenario where current_tick is peak
+        if tickers[1].max() <= eth_val:
+            if tick.p.eth_btc.volume * 0.5 > trans_tracker.get_holdings()["ETH"]:
+                trans_tracker.make_trade(current_tick, 'ETH-BTC', 'sell', -1)
+            else:
+                trans_tracker.make_trade(current_tick, 'ETH-BTC', 'sell', tick.p.eth_btc.volume * 0.4999999)
+    if checkIfLTC():
+        # Scenario where current_tick is peak
+        if tickers[2].max() <= ltc_val:
+            if tick.p.ltc_btc.volume * 0.5 > trans_tracker.get_holdings()["LTC"]:
+                trans_tracker.make_trade(current_tick, 'LTC-BTC', 'sell', -1)
+            else:
+                trans_tracker.make_trade(current_tick, 'LTC-BTC', 'sell', tick.p.ltc_btc.volume * 0.4999999)
+
+
+def model_predictions(frame):
+    step = 25
+    return np.stack((1/df_usd['close'].values[frame:frame+step],
+                    df_eth['close'].values[frame:frame+step],
+                    df_ltc['close'].values[frame:frame+step]), 0)
 
 
 tick = tick_gen.get_tick()
 last_price = tick.p.btc_usd.close
 
-
+frame = 0
 while tick is not None:
     tick = tick_gen.get_tick()
+    frame += 1
     if tick is not None:
         #TODO get predicted tickers from NeuralNetwork
-        makeTrade(isHolding, tick, up_slope)
+        makeTrade(tick, model_predictions(frame), last_price)
 trans_tracker.plot_btc_holdings()
+print(trans_tracker.get_holdings())
 print(trans_tracker.get_sharpe_ratio())
 trans_tracker.dump_trades('example_trades.json')
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -4,27 +4,33 @@ import numpy as np
 from TDSCoinbaseData import TDSCoinbaseData
 
 
-class GRU(nn.Module):
+class FF(nn.Module):
     def __init__(self):
-        super(GRU, self).__init__()
-        self.hidden_dim = 160
-        self.num_layers = 3
+        super(FF, self).__init__()
 
-        self.gru = nn.GRU(5, self.hidden_dim, self.num_layers, batch_first=True)
-        self.fc = nn.Linear(self.hidden_dim, 5)
+        self.fc1 = nn.Linear(240, 160)
+        self.fc2 = nn.Linear(160, 80)
+        self.fc3 = nn.Linear(80, 40)
+        self.fc4 = nn.Linear(40, 4)
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
-        out, (hn) = self.gru(x, (h0.detach()))
-        out = self.fc(out[:, -1, :])
+
+        out = x.reshape((1, 1, -1))
+        out = self.fc1(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        out = self.fc4(out)
         return out
 
 
-def train_model(epochs, product='BTC-USD'):
-    model = GRU()
-    criterion = torch.nn.MSELoss(reduction='mean')
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-    batch_size = 16
+def train_model(load_path, save_path, epochs, product='BTC-USD'):
+    model = FF()
+    if load_path != "":
+        model.load_state_dict(torch.load(load_path))
+
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    batch_size = 64
 
     cb = TDSCoinbaseData()
     start_date = '20200101'
@@ -35,24 +41,55 @@ def train_model(epochs, product='BTC-USD'):
     tensor_high = torch.tensor(df['high'].values)
     tensor_open = torch.tensor(df['open'].values)
     tensor_close = torch.tensor(df['close'].values)
-    tensor_volume = torch.tensor(df['volume'].values)
-    tensor_data = torch.stack([tensor_low, tensor_high, tensor_open, tensor_close, tensor_volume], 1)
-    print(tensor_data[0])
+    tensor_data = torch.stack([tensor_low, tensor_high, tensor_open, tensor_close], 1).reshape((1, -1, 4)).float()
+    print(tensor_data[:, 1:2, :].shape)
     print(tensor_data.shape)
 
     for t in range(epochs):
+        avg_loss = torch.zeros(1)
+        for i in range(tensor_data.shape[1] - 60):
+            output = model(tensor_data[:, i:i+60, :])
+            loss = criterion(output, tensor_data[:, i+60:i+61, :])
+            avg_loss += loss
 
-        batch = 0
-        for i in range(tensor_data.shape[0]-1):
-            print('.')
-            output = model(tensor_data[i])
-            loss = criterion(output, tensor_data[i+1])
-            batch += 1
-            if batch >= batch_size:
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                batch = 0
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print("AVG LOSS")
+        print(avg_loss / (tensor_data.shape[1] - 60))
+        print("SAVING")
+        torch.save(model.state_dict(), save_path)
 
 
-train_model(50)
+def test_model(path, product='BTC-USD'):
+    model = FF()
+    model.load_state_dict(torch.load(path))
+
+    criterion = torch.nn.L1Loss()
+
+    cb = TDSCoinbaseData()
+    start_date = '20200601'
+    end_date = '20200630'
+
+    df = cb.get_market_data(product, start_date, end_date, interval=60)
+    tensor_low = torch.tensor(df['low'].values)
+    tensor_high = torch.tensor(df['high'].values)
+    tensor_open = torch.tensor(df['open'].values)
+    tensor_close = torch.tensor(df['close'].values)
+    tensor_data = torch.stack([tensor_low, tensor_high, tensor_open, tensor_close], 1).reshape(
+        (1, -1, 4)).float()
+
+    avg_loss = torch.zeros(1)
+    for i in range(tensor_data.shape[1] - 60):
+        output = model(tensor_data[:, i:i + 60, :])
+        print(output[:, :, 3], tensor_data[:, i + 60:i + 61, 3])
+        loss = criterion(output[:, :, 3], tensor_data[:, i + 60:i + 61, 3])
+        avg_loss += loss
+    avg_loss /= (tensor_data.shape[1] - 60)
+    print("AVG LOSS: ")
+    print(avg_loss)
+
+
+train_model("btc_usd.pth", "btc_usd2.pth", 50)
+#test_model("btc_usd2.pth")
